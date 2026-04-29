e1e625258c1fa69786a939ea060780f9cbed8397
Robinson Ordonez <robinsonordonezbarreiro@gmail.com>
2026-04-29 14:20:47 +0200

    Vinculación CFA - MD

--- src/config/global.js
+++ src/config/global.js
@@ -313,7 +313,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/228147_CF07_DU.zip',
       },
       {
         icono: 'fas fa-download',
